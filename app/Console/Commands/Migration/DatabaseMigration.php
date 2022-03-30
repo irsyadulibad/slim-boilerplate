4b2c5a3cf2a582bf25c0fee0f6105bd65ec7ca47
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-03-30 07:14:34 +0700

    chore: skip migrated files

--- app/Console/Commands/Migration/DatabaseMigration.php
+++ app/Console/Commands/Migration/DatabaseMigration.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands\Migration;
 
 use App\Support\MigrationSupport;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Schema\Blueprint;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -16,17 +18,48 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class DatabaseMigration extends Command
 {
+    protected function configure()
+    {
+        $this->createMigrationTable();
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $migrations = MigrationSupport::migrations();
+        $migratedFiles = $this->getMigratedFiles();
 
         foreach($migrations as $migration) {
+            if(in_array($migration->name, $migratedFiles)) continue;
+
             require $migration->path;
             (new ("\Database\\Migrations\\{$migration->class}"))->up();
+            $this->saveMigration($migration->name);
 
             $output->writeln("Migrated: <info>{$migration->name}</info>");
         }
 
         return Command::SUCCESS;
+    }
+
+    private function createMigrationTable()
+    {
+        $schema = Capsule::schema();
+
+        if(!$schema->hasTable('migrations')) {
+            $schema->create('migrations', function(Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('migration');
+            });
+        }
+    }
+
+    private function saveMigration(string $migration): void
+    {
+        Capsule::table('migrations')->insert(compact('migration'));
+    }
+
+    private function getMigratedFiles(): array
+    {
+        return Capsule::table('migrations')->pluck('migration')->toArray();
     }
 }
